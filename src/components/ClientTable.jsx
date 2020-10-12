b2b14433dd42f8d5c90f99f764e953e849a3700c
Yooshin Yong <yong.yooshin@gmail.com>
2020-10-12 19:56:02 +1100

    UPDATE to have columns and REMOVE unecessary fields from data file

--- src/components/ClientTable.jsx
+++ src/components/ClientTable.jsx
@@ -1,18 +1,46 @@
 import React from 'react';
 // import { DataGrid } from '@material-ui/data-grid';
-import { Table, TableBody, TableRow, TableCell} from '@material-ui/core'
+import { Table, TableHead, TableBody, TableRow, TableCell} from '@material-ui/core'
 import clientData from '../data/clients.json';
 
 const ClientTable = props => {
+    const columnNames = [
+        "Client Name",
+        "Email",
+        "Phone",
+        "Industry",
+        "Point of Contact",
+        "Website"
+    ];
 
+    const fieldList = [
+        "clientName",
+        "clientEmail",
+        "clientPersonalPhone",
+        "clientIndustry",
+        "clientPocName"
+    ];
     return ( <Table>
+
+        {/* Headers */}
+        <TableHead>
+            <TableRow>
+                { columnNames.map( currColumn => {
+                    return (<TableCell>
+                        {currColumn}
+                    </TableCell>);
+                })}
+            </TableRow>
+        </TableHead>    
+
+        {/* Body */}
         <TableBody>
             { clientData.map( (currData, indexI) => {
                 return (currData && <TableRow key={`ClientRow${indexI}`} > 
                 
-                    { Object.keys(currData).map( (currKey) => {
-                        return (<TableCell key={`ClientRow${indexI}-${currKey}`} >
-                            {currData[currKey]}
+                    { fieldList.map( (currField) => {
+                        return (<TableCell key={`ClientRow${indexI}-${currField}`} >
+                            {currData[currField]}
                         </TableCell>);
                     })}
                 </TableRow>); 
