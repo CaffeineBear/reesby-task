328328e5c12e7218cd7f5e78312e943946b0ce9d
Yooshin Yong <yong.yooshin@gmail.com>
2020-10-12 21:37:33 +1100

    UPDATE to insert cell selection logic.

--- src/components/ClitentTable/index.jsx
+++ src/components/ClitentTable/index.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
-import { Table, TableHead, TableBody, TableRow, TableCell} from '@material-ui/core'
+import React, { useState, useEffect } from 'react';
+import { 
+    Checkbox,
+    Table, TableHead, TableBody, TableRow, TableCell
+} from '@material-ui/core'
 import clientData from '../../data/clients.json';
 
 const ClientTable = props => {
@@ -19,13 +22,40 @@ const ClientTable = props => {
         "clientIndustry",
         "clientPocName"
     ];
+
+    const rowCount = clientData.length;
+    const [ selected,    updateSelected    ] = useState(new Array(rowCount).fill(false) );
+    const [ numSelected, updateNumSelected ] = useState(0);
+
+    const onSelectClick = (index) => {
+        updateSelected( prevSelected => { 
+            prevSelected[index] = !prevSelected[index]; 
+            return prevSelected;
+        });
+        updateNumSelected( prevNumSelected => prevNumSelected + 1 );
+    }
+    
+    const onSelectAllClick = () => {
+        const toggleSelect = !(numSelected === rowCount);
+        updateSelected( new Array(rowCount).fill(toggleSelect) );
+        updateNumSelected( toggleSelect ? rowCount : 0 );
+    }
+
     return ( <Table>
 
         {/* Headers */}
         <TableHead>
             <TableRow>
+                <TableCell padding="checkbox">
+                    <Checkbox
+                        indeterminate={numSelected > 0 && numSelected < rowCount}
+                        checked={rowCount > 0 && numSelected === rowCount}
+                        onChange={onSelectAllClick}
+                        inputProps={{ 'aria-label': 'select all clients' }}
+                    />
+                </TableCell>
                 { columnNames.map( currColumn => {
-                    return (<TableCell>
+                    return (<TableCell key={`ClientTableColumn${currColumn}`}>
                         {currColumn}
                     </TableCell>);
                 })}
@@ -37,6 +67,16 @@ const ClientTable = props => {
             { clientData.map( (currData, indexI) => {
                 return (currData && <TableRow key={`ClientRow${indexI}`} > 
                 
+                    {/* Checkbox */}
+                    <TableCell padding="checkbox"  key={`ClientRow${indexI}-Checkbox`}>
+                        <Checkbox
+                            checked={selected[indexI]}
+                            onChange={ () => {onSelectClick(indexI) } }
+                            inputProps={{ 'aria-label': 'select current client' }}
+                        />
+                    </TableCell>
+
+                    {/* Table Contents */}
                     { fieldList.map( (currField) => {
                         return (<TableCell key={`ClientRow${indexI}-${currField}`} >
                             {currData[currField]}
