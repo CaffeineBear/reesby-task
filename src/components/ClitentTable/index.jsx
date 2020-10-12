3451fdfb3163d36878e71429802ac8b5434ac79f
Yooshin Yong <yong.yooshin@gmail.com>
2020-10-13 08:53:29 +1100

    UPDATE to have tab space of 4 in ClientTable component

--- src/components/ClitentTable/index.jsx
+++ src/components/ClitentTable/index.jsx
@@ -1,91 +1,91 @@
 import React, { useState, useEffect } from 'react';
-import { 
-    Checkbox,
-    Table, TableHead, TableBody, TableRow, TableCell
+import {
+  Checkbox,
+  Table, TableHead, TableBody, TableRow, TableCell
 } from '@material-ui/core'
 import clientData from '../../data/clients.json';
 
 const ClientTable = props => {
-    const columnNames = [
-        "Client Name",
-        "Email",
-        "Phone",
-        "Industry",
-        "Point of Contact",
-        "Website"
-    ];
+  const columnNames = [
+    "Client Name",
+    "Email",
+    "Phone",
+    "Industry",
+    "Point of Contact",
+    "Website"
+  ];
 
-    const fieldList = [
-        "clientName",
-        "clientEmail",
-        "clientPersonalPhone",
-        "clientIndustry",
-        "clientPocName"
-    ];
+  const fieldList = [
+    "clientName",
+    "clientEmail",
+    "clientPersonalPhone",
+    "clientIndustry",
+    "clientPocName"
+  ];
 
-    const rowCount = clientData.length;
-    const [ selected,    updateSelected    ] = useState(new Array(rowCount).fill(false) );
-    const [ numSelected, updateNumSelected ] = useState(0);
+  const rowCount = clientData.length;
+  const [selected, updateSelected] = useState(new Array(rowCount).fill(false));
+  const [numSelected, updateNumSelected] = useState(0);
 
-    const onSelectClick = (index) => {
-        updateSelected( prevSelected => { 
-            prevSelected[index] = !prevSelected[index]; 
-            return prevSelected;
-        });
-        updateNumSelected( prevNumSelected => prevNumSelected + 1 );
-    }
-    
-    const onSelectAllClick = () => {
-        const toggleSelect = !(numSelected === rowCount);
-        updateSelected( new Array(rowCount).fill(toggleSelect) );
-        updateNumSelected( toggleSelect ? rowCount : 0 );
-    }
+  const onSelectClick = (index) => {
+    updateSelected(prevSelected => {
+      prevSelected[index] = !prevSelected[index];
+      return prevSelected;
+    });
+    updateNumSelected(prevNumSelected => prevNumSelected + 1);
+  }
 
-    return ( <Table>
+  const onSelectAllClick = () => {
+    const toggleSelect = !(numSelected === rowCount);
+    updateSelected(new Array(rowCount).fill(toggleSelect));
+    updateNumSelected(toggleSelect ? rowCount : 0);
+  }
 
-        {/* Headers */}
-        <TableHead>
-            <TableRow>
-                <TableCell padding="checkbox">
-                    <Checkbox
-                        indeterminate={numSelected > 0 && numSelected < rowCount}
-                        checked={rowCount > 0 && numSelected === rowCount}
-                        onChange={onSelectAllClick}
-                        inputProps={{ 'aria-label': 'select all clients' }}
-                    />
-                </TableCell>
-                { columnNames.map( currColumn => {
-                    return (<TableCell key={`ClientTableColumn${currColumn}`}>
-                        {currColumn}
-                    </TableCell>);
-                })}
-            </TableRow>
-        </TableHead>    
+  return (<Table>
 
-        {/* Body */}
-        <TableBody>
-            { clientData.map( (currData, indexI) => {
-                return (currData && <TableRow key={`ClientRow${indexI}`} > 
-                
-                    {/* Checkbox */}
-                    <TableCell padding="checkbox"  key={`ClientRow${indexI}-Checkbox`}>
-                        <Checkbox
-                            checked={selected[indexI]}
-                            onChange={ () => {onSelectClick(indexI) } }
-                            inputProps={{ 'aria-label': 'select current client' }}
-                        />
-                    </TableCell>
+    {/* Headers */}
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Checkbox
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{ 'aria-label': 'select all clients' }}
+          />
+        </TableCell>
+        {columnNames.map(currColumn => {
+          return (<TableCell key={`ClientTableColumn${currColumn}`}>
+            {currColumn}
+          </TableCell>);
+        })}
+      </TableRow>
+    </TableHead>
 
-                    {/* Table Contents */}
-                    { fieldList.map( (currField) => {
-                        return (<TableCell key={`ClientRow${indexI}-${currField}`} >
-                            {currData[currField]}
-                        </TableCell>);
-                    })}
-                </TableRow>); 
-            }) }
-        </TableBody>
-    </Table> );
+    {/* Body */}
+    <TableBody>
+      {clientData.map((currData, indexI) => {
+        return (currData && <TableRow key={`ClientRow${indexI}`} >
+
+          {/* Checkbox */}
+          <TableCell padding="checkbox" key={`ClientRow${indexI}-Checkbox`}>
+            <Checkbox
+              checked={selected[indexI]}
+              onChange={() => { onSelectClick(indexI) }}
+              inputProps={{ 'aria-label': 'select current client' }}
+            />
+          </TableCell>
+
+          {/* Table Contents */}
+          {fieldList.map((currField) => {
+            return (<TableCell key={`ClientRow${indexI}-${currField}`} >
+              {currData[currField]}
+            </TableCell>);
+          })}
+        </TableRow>);
+      })}
+    </TableBody>
+  </Table>);
 }
 
 export default ClientTable;     
